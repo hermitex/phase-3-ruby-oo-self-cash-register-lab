4a63567eabef5eed1d149deecd5842601262a546
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-25 09:10:21 +0300

    commit tree b4a50cdfe1c052d3510a34e5d963c24235a3c92e

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class CashRegister
+  attr_accessor :total, :discount, :items
+  @@total = 0
+  @items = []
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
 
+  def add_item(title, price, quantity=0)
+    self.total += price
+    @quantity = quantity
+    @items << title
+  end
+
+  def discount
+    @discount > 0 ? @discount  - self.total : nil
+  end
+
+  def total
+    @total
+  end
+
+  def items
+    @items
+  end
+end
